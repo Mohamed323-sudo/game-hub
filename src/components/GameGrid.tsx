93ee9b0b4f2e93eef57f98f3f15686188c5b309a
Mohamed said abdirahaman <saciidm088@gmail.com>
2024-08-18 09:53:31 +0600

    Display Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,11 +10,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        spacing={10}
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-      >
+      <SimpleGrid spacing={3} columns={{ sm: 1, md: 2, lg: 3 }} padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>
